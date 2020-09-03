d05e2709aff620921215a695c4fafc9fa6d65d2a
Saishashankd <68693182+saishashankd@users.noreply.github.com>
2020-09-03 05:58:15 +0530

    Update nameFunction.py

--- nameFunction.py
+++ nameFunction.py
@@ -1,25 +1,13 @@
 def greet(name):
-    """
-    This function greets to
-    the person passed in as
-    a parameter
-    """
     print("Hello, " + name + ". Good afternoon!")
     
 def greeteve(name):
-    """
-    This function greets to
-    the person passed in as
-    a parameter
-    """
     print("Hello, " + name + ". Good Evening !")
 
     
 def greetmor(name):
     """
-    This function greets to
-    the person passed in as
-    a parameter
+    This function executes other fuctions and its own
     """
     greet('saishashank')
     greeteve('shashank')

9a125e6550c6d1e57562dd84f9b735be1124ddaf
Saishashankd <68693182+saishashankd@users.noreply.github.com>
2020-09-03 05:52:34 +0530

    added two more functions

--- nameFunction.py
+++ nameFunction.py
@@ -5,3 +5,22 @@ def greet(name):
     a parameter
     """
     print("Hello, " + name + ". Good afternoon!")
+    
+def greeteve(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    """
+    print("Hello, " + name + ". Good Evening !")
+
+    
+def greetmor(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    """
+    greet('saishashank')
+    greeteve('shashank')
+    print("Hello, " + name + ". Good Morning!")
